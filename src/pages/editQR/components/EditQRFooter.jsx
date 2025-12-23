8df49ae9311fc6e2f662a1d2f70c631af47924e1
Digital23 <digital23@iMac-de-Digital23.local>
2025-12-23 08:49:46 -0300

    save and cancel added to EditQR.jsx

--- src/pages/editQR/components/EditQRFooter.jsx
+++ src/pages/editQR/components/EditQRFooter.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom"
+
 const EditQRFooter = ({ onSave }) => {
+
+const navigate = useNavigate(onSave);
+
   return (
     <footer className="edit-qr-footer">
-        <button className="cancel-button">Cancelar</button>
+        <button className="cancel-button" onClick={() => navigate(-1)}>Cancelar</button>
         <button className="save-button" onClick={onSave}>
           Guardar cambios
         </button>
